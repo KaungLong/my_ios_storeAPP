5ca2c50749fd80de5f7ce6a17ad90a0e1ddf3e91
WeiMoKaungLong <infinity19971023@gmail.com>
2023-02-09 15:45:01 +0800

    Initial Commit

---
+++ my_ios_storeAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my_ios_storeAPP
+//
+//  Created by 危末狂龍 on 2023/2/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
